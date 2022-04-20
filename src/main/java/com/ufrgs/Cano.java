51588031a2679e6d8942197ac421c1917aa15bb3
Leonardo <dw.leonardo1@gmail.com>
2022-04-19 22:30:54 -0300

    adicionado classe cano

---
+++ src/main/java/com/ufrgs/Cano.java
@@ -0,0 +1,32 @@
+package com.ufrgs;
+
+public class Cano extends ElementoGrafico{
+    
+    private Cenario superior;
+    private Cenario inferior;
+
+    public Cano(int semente) {
+        super("src/main/resources/troncoinf.png", 1.5);
+        superior = new Cenario("src/main/resources/tronco.png", 0.3, 1.5);
+        inferior = new Cenario("src/main/resources/tronco.png", 0.3, 1.5);
+        superior.mover(1280, 0);
+        inferior.mover(1280, 470);
+    }
+
+    public void atualizar(){
+        superior.atualizar();
+        inferior.atualizar();
+    }
+
+    public void renderizar() {
+        superior.renderizar();
+        inferior.renderizar();
+    }
+
+    public void descarregar() {
+        super.descarregar();
+        superior.descarregar();
+        inferior.descarregar();
+    }
+
+}
